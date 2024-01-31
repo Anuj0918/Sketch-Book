51d745f2abf6179866f94358fd43ae7767f1a8bd
Anuj <anujtiwari2822@gmail.com>
2024-01-31 11:38:04 +0530

    setup redux

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,16 +1,26 @@
-
+import { useDispatch,useSelector } from 'react-redux'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPencil, faEraser, faRotateLeft, faRotateRight, faFileArrowDown, faPencilAlt } from '@fortawesome/free-solid-svg-icons'
 
 import styles from './index.module.css'
- 
+import { menuItemClick,actionItemClick } from '@/slice/menuSlice'
+ import { MENU_ITEMS } from '@/constants'
  const Menu = () => {
+       
+      const dispatch = useDispatch()
+      const activeMenuItem = useSelector((state) => state.menu.activeMenuItem)
+      const handleMenuClick = (itemName) => {
+            dispatch(menuItemClick(itemName))
+        }
+        const handleActioItemClick = (itemName) => {
+            dispatch(actionItemClick(itemName))
+        }
      return (
         <div className={styles.menuContainer}>
-            <div className={styles.iconWrapper}>
+            <div className={styles.iconWrapper} onClick={() => handleMenuClick(MENU_ITEMS.PENCIL)}>
                 <FontAwesomeIcon icon={faPencil} className={styles.icon}/>
           </div>
-          <div className={styles.iconWrapper}>
+          <div className={styles.iconWrapper} onClick={() => handleMenuClick(MENU_ITEMS.ERASER)}>
                 <FontAwesomeIcon icon={faEraser} className={styles.icon}/>
           </div>
           <div className={styles.iconWrapper}>
